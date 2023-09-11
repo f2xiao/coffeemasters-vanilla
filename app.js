80b40e7c68ae27d1f7e3f9c6958e7599ca116145
f2xiao <25191617+f2xiao@users.noreply.github.com>
2023-09-11 12:56:04 -0400

    Edit app.js

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-const $ = function(args){ return document.querySelector(args);}
-const $$ = function(args){ return document.querySelectorAll(args);}
-
-HTMLElement.prototype.on = function(a, b, c){ return this.addEventListener(a, b, c); }
-HTMLElement.prototype.off = function(a, b){ return this.removeEventListener(a, b); }
-HTMLElement.prototype.$ = function(s){ return this.querySelector(s); }
-HTMLElement.prototype.$$ = function(s){ return this.querySelectorAll(s); }
-
 window.addEventListener("DOMContentLoaded", () => {
-    console.log("DOM is ready");
-    console.log($$('body .navlink'));
+    loadData();
 });
 
 import Store from './services/Store.js';
 import API from './services/API.js';
+import { loadData } from "./services/Menu.js";
 
 window.app = {}
 app.store = Store;
+
+
+
