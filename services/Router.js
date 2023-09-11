895d9d51f0e448b72e2d968e59b019213bfdfb3f
f2xiao <25191617+f2xiao@users.noreply.github.com>
2023-09-11 15:08:50 -0400

    Edit services/Router.js

--- services/Router.js
+++ services/Router.js
@@ -1,8 +1,49 @@
 const Router = {
     init: () => {
+        document.querySelectorAll("a.navlink").forEach(a => {
+            a.addEventListener("click", event => {
+                event.preventDefault();
+                const href = event.target.getAttribute("href");
+                Router.go(href);
+            });
+        });  
+        // Process initial URL   
+        Router.go(location.pathname);
+
+        window.addEventListener('popstate',  event => {
+            Router.go(event.state.route, false);
+        });
         
     },    
     go: (route, addToHistory=true) => {
+        // console.log(`Going to ${route}`)
+        if (addToHistory) {
+            history.pushState({ route }, '', route);
+        }
+        let pageElement = null;
+        switch (route) {
+            case "/":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Menu";
+                break;
+            case "/order":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Your order";
+                break;
+            default:
+                if (route.startsWith("/product-")) {                
+                    pageElement = document.createElement("h1");
+                    pageElement.textContent = "Details";
+                    pageElement.dataset.productId = route.substring(route.lastIndexOf("-")+1);
+                }
+                break;   
+        }
+        if (pageElement) {
+            document.querySelector("main").innerHTML = "";
+            document.querySelector("main").appendChild(pageElement);
+        }
+    
+        window.scrollX = 0;
 
     }
 }
