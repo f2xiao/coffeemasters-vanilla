dbd2f11030c3b8f896de01fe7357c050ac2f3b3f
f2xiao <25191617+f2xiao@users.noreply.github.com>
2023-09-11 14:27:34 -0400

    Edit app.js

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 import Store from './services/Store.js';
 import API from './services/API.js';
 import { loadData } from "./services/Menu.js";
+import Router from './services/Router.js';
 
 window.app = {}
 app.store = Store;
+app.router = Router;
 
 app.renderTest = function renderTest(){
-    console.log(app.store.menu);
+    app.store.menu.forEach(ele => {
+        console.log(ele.name);
+    });
+    
 }
 
 window.addEventListener("DOMContentLoaded", () => {
+    app.router.init();
     loadData();
 });
 
