fe3f6804860cde8076232ef616ca132e5a3fe463
f2xiao <25191617+f2xiao@users.noreply.github.com>
2023-09-12 14:52:17 -0400

    Edit DetailsPage.js

--- components/DetailsPage.js
+++ components/DetailsPage.js
@@ -1,6 +1,23 @@
+async function loadCSS(ele) {
+    const request = await fetch("/components/DetailsPage.css");
+    ele.styles.textContent = await request.text();
+}
 export default class DetailsPage extends HTMLElement {
     constructor() {
         super();
+
+         // Implement a Shadow DOM on every Custom Elements
+         const shadowRoot = this.attachShadow({ mode: "open" });
+
+         const template = document.getElementById("details-page-template");
+         const content = template.content.cloneNode(true);
+         this.styles = shadowRoot.appendChild(document.createElement('style'));
+         shadowRoot.appendChild(content);
+    }
+
+    connectedCallback(){
+        console.log(this);
+        loadCSS(this);
     }
 }
 customElements.define("details-page", DetailsPage);
