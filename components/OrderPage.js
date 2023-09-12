1737bf879e23ba868c004cbb9dc046572d9eae15
f2xiao <25191617+f2xiao@users.noreply.github.com>
2023-09-12 14:53:56 -0400

    Edit components/OrderPage.js

--- components/OrderPage.js
+++ components/OrderPage.js
@@ -1,13 +1,23 @@
+async function loadCSS(ele) {
+    const request = await fetch("/components/OrderPage.css");
+    ele.styles.textContent = await request.text();
+}
 export default class OrderPage extends HTMLElement {
     constructor() {
         super();
 
         // Implement a Shadow DOM on every Custom Elements
-        this.root = this.attachShadow({ mode: "open" });
-
+        const shadowRoot = this.attachShadow({ mode: "open" });
+        
         const template = document.getElementById("order-form-template");
         const content = template.content.cloneNode(true);
-        this.root.appendChild(content);  
+        this.styles = shadowRoot.appendChild(document.createElement('style'));
+        shadowRoot.appendChild(content);
+    }
+
+    connectedCallback(){
+        // console.log(this);
+        loadCSS(this); 
     }
 }
 customElements.define("order-page", OrderPage);
