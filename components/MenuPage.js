973f3ef6dfdf8ecd3c9c8c6c334eca93460029b1
f2xiao <25191617+f2xiao@users.noreply.github.com>
2023-09-13 15:56:41 -0400

    Register appmenuchange event and its handler

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -4,7 +4,7 @@ export default class MenuPage extends HTMLElement {
         super();
 
         // Implement a Shadow DOM on every Custom Elements
-        const shadowRoot = this.attachShadow({ mode: "open" });
+        this.root = this.attachShadow({ mode: "open" });
         
         async function loadCSS() {
             const request = await fetch("/components/MenuPage.css");
@@ -19,11 +19,37 @@ export default class MenuPage extends HTMLElement {
         const template = document.getElementById("menu-page-template");
         const content = template.content.cloneNode(true);
         content.appendChild(styles);
-        shadowRoot.appendChild(content);
+        this.root.appendChild(content);
+    }
+
+    render() {
+        console.log(app.store.menu)
+        if (app.store.menu) {
+            this.root.querySelector("#menu").innerHTML = "";
+            for (let category of app.store.menu) {
+                const liCategory = document.createElement("li");
+                liCategory.innerHTML = `
+                    <h3>${category.name}</h3>
+                    <ul class='category'>
+                    </ul>`;
+                this.root.querySelector("#menu").appendChild(liCategory);
+    
+                // TODO: placeholder
+
+            }  
+        } else {
+            this.root.querySelector("#menu").innerHTML = `Loading...`;
+        }
     }
 
     connectedCallback(){
-       console.log(this);
+       console.log(this.root);
+        // the menupage re-render if the "appmenuchange" event is fired
+        window.addEventListener("appmenuchange", () => {
+            this.render();
+        });
+        // initial render
+        this.render();
     }
 }
 customElements.define("menu-page", MenuPage);
